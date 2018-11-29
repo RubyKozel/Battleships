3e6df7c2f1f0481cb66f0dffce975eb00c0e030f
Ruby_Kozel <rubykozel@gmail.com>
2018-11-29 21:13:05 +0200

    Added class AnimationHandler

---
+++ app/src/main/java/com/example/kozel/battleship/AnimationHandler.java
@@ -0,0 +1,112 @@
+package com.example.kozel.battleship;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Handler;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+class AnimationHandler {
+    private Handler handler;
+    private GridView humanView;
+    private GridView computerView;
+    private ProgressBar progressBar;
+    private LinearLayout computersShipsLeft;
+    private LinearLayout humanShipsLeft;
+    private TextView turnDisplayer;
+    private static final int DURATION = 1500;
+    private static final int DELAY = 500;
+    private static final int LOW_OPAQUE = 0;
+    private static final int HIGH_OPAQUE = 1;
+
+    AnimationHandler(
+            GridView humanView,
+            GridView computerView,
+            LinearLayout computersShipsLeft,
+            LinearLayout humanShipsLeft,
+            ProgressBar progressBar,
+            TextView turnDisplayer) {
+        this.humanView = humanView;
+        this.computersShipsLeft = computersShipsLeft;
+        this.computerView = computerView;
+        this.humanShipsLeft = humanShipsLeft;
+        this.progressBar = progressBar;
+        this.turnDisplayer = turnDisplayer;
+        handler = new Handler();
+    }
+
+    public void toggleHumanVisibility() {
+        handler.postDelayed(() -> {
+            toggleTurnDisplayAnimation(R.string.computer_turn_display);
+            toggleVisibilityAnimation(computerView, View.INVISIBLE);
+            toggleVisibilityAnimation(computersShipsLeft, View.INVISIBLE);
+            toggleProgressBarVisible();
+        }, DELAY);
+
+        handler.postDelayed(() -> {
+            toggleVisibilityAnimation(humanView, View.VISIBLE);
+            toggleVisibilityAnimation(humanShipsLeft, View.VISIBLE);
+        }, DURATION);
+    }
+
+    public void toggleComputerVisibility() {
+        handler.postDelayed(() -> {
+            toggleTurnDisplayAnimation(R.string.human_turn_display);
+            toggleVisibilityAnimation(humanView, View.INVISIBLE);
+            toggleVisibilityAnimation(humanShipsLeft, View.INVISIBLE);
+        }, DELAY);
+
+        handler.postDelayed(() -> {
+            toggleVisibilityAnimation(computerView, View.VISIBLE);
+            toggleVisibilityAnimation(computersShipsLeft, View.VISIBLE);
+        }, DURATION);
+    }
+
+    private void toggleVisibilityAnimation(View view, int visibility) {
+        view.setAlpha(visibility == View.VISIBLE ? LOW_OPAQUE : HIGH_OPAQUE);
+        if (visibility == View.VISIBLE)
+            view.setVisibility(visibility);
+        view.animate()
+                .setDuration(DURATION)
+                .alpha(visibility == View.VISIBLE ? HIGH_OPAQUE : LOW_OPAQUE)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        view.setVisibility(visibility);
+                    }
+                });
+    }
+
+    private void toggleTurnDisplayAnimation(int idOfString) {
+        turnDisplayer.setAlpha(HIGH_OPAQUE);
+        turnDisplayer.animate().setDuration(DURATION / 2).alpha(LOW_OPAQUE).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                turnDisplayer.setAlpha(0.0f);
+                turnDisplayer.setText(idOfString);
+                turnDisplayer.animate().setDuration(DURATION / 2).alpha(HIGH_OPAQUE);
+                turnDisplayer.animate().setListener(null);
+            }
+        });
+    }
+
+    private void toggleProgressBarVisible() {
+        progressBar.animate().setDuration(DURATION).alpha(HIGH_OPAQUE).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+     void toggleProgressBarInvisible() {
+        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.setAlpha(LOW_OPAQUE);
+    }
+}
--- app/src/main/java/com/example/kozel/battleship/BoardsActivity.java
+++ app/src/main/java/com/example/kozel/battleship/BoardsActivity.java
@@ -1,8 +1,5 @@
 package com.example.kozel.battleship;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,33 +16,30 @@ public class BoardsActivity extends AppCompatActivity {
 
     private BattleshipController controller;
     private Difficulty difficulty;
-    private Handler handler;
     private GridView humanView;
     private GridView computerView;
-    private ProgressBar progressBar;
     private LinearLayout computersShipsLeft;
     private LinearLayout humanShipsLeft;
-    public static final int DURATION = 1500;
-    public static final int DELAY = 500;
-    public static final int LOW_OPAQUE = 0;
-    public static final int HIGH_OPAQUE = 1;
+    private AnimationHandler animationHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boards);
 
-        handler = new Handler();
-
-        progressBar = findViewById(R.id.progressBar);
         humanView = findViewById(R.id.human_grid);
         computerView = findViewById(R.id.computer_grid);
         computersShipsLeft = findViewById(R.id.shipsCountLayoutComputer);
         humanShipsLeft = findViewById(R.id.shipsCountLayoutHuman);
 
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+        TextView turnDisplayer = findViewById(R.id.turnView);
+
         computersShipsLeft.setBackgroundResource(R.drawable.border);
         humanShipsLeft.setBackgroundResource(R.drawable.border);
-        progressBar.setVisibility(View.INVISIBLE);
+
+        animationHandler = new AnimationHandler(humanView, computerView, computersShipsLeft, humanShipsLeft, progressBar, turnDisplayer);
 
         Bundle b = getIntent().getBundleExtra(MainActivity.BUNDLE_KEY);
 
@@ -96,7 +90,7 @@ public class BoardsActivity extends AppCompatActivity {
         if (controller.humanPlay(position)) {
             ((TileAdapter) computerView.getAdapter()).notifyDataSetChanged();
             refreshShipAmount(computersShipsLeft, controller.getComputerBoard().getShipAmounts());
-            toggleHumanVisibility();
+            animationHandler.toggleHumanVisibility();
         }
     }
 
@@ -104,10 +98,10 @@ public class BoardsActivity extends AppCompatActivity {
         new Thread(() -> {
             controller.computerPlay();
             runOnUiThread(() -> {
-                toggleProgressBarInvisible();
+                animationHandler.toggleProgressBarInvisible();
                 ((TileAdapter) humanView.getAdapter()).notifyDataSetChanged();
                 refreshShipAmount(humanShipsLeft, controller.getHumanBoard().getShipAmounts());
-                toggleComputerVisibility();
+                animationHandler.toggleComputerVisibility();
             });
         }).start();
     }
@@ -119,78 +113,5 @@ public class BoardsActivity extends AppCompatActivity {
                     .getChildAt(i))
                     .setText(getResources().getString(R.string.sized, shipAmounts[count - i - 1]));
         }
-    }
-
-    private void toggleHumanVisibility() {
-        handler.postDelayed(() -> {
-            toggleTurnDisplayAnimation(R.string.computer_turn_display);
-            toggleVisibilityAnimation(computerView, View.INVISIBLE);
-            toggleVisibilityAnimation(computersShipsLeft, View.INVISIBLE);
-            toggleProgressBarVisible();
-        }, DELAY);
-
-        handler.postDelayed(() -> {
-            toggleVisibilityAnimation(humanView, View.VISIBLE);
-            toggleVisibilityAnimation(humanShipsLeft, View.VISIBLE);
-        }, DURATION);
-    }
-
-    private void toggleComputerVisibility() {
-        handler.postDelayed(() -> {
-            toggleTurnDisplayAnimation(R.string.human_turn_display);
-            toggleVisibilityAnimation(humanView, View.INVISIBLE);
-            toggleVisibilityAnimation(humanShipsLeft, View.INVISIBLE);
-        }, DELAY);
-
-        handler.postDelayed(() -> {
-            toggleVisibilityAnimation(computerView, View.VISIBLE);
-            toggleVisibilityAnimation(computersShipsLeft, View.VISIBLE);
-        }, DURATION);
-    }
-
-    private void toggleVisibilityAnimation(View view, int visibility) {
-        view.setAlpha(visibility == View.VISIBLE ? LOW_OPAQUE : HIGH_OPAQUE);
-        if (visibility == View.VISIBLE)
-            view.setVisibility(visibility);
-        view.animate()
-                .setDuration(DURATION)
-                .alpha(visibility == View.VISIBLE ? HIGH_OPAQUE : LOW_OPAQUE)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        super.onAnimationEnd(animation);
-                        view.setVisibility(visibility);
-                    }
-                });
-    }
-
-    private void toggleTurnDisplayAnimation(int idOfString) {
-        TextView v1 = findViewById(R.id.turnView);
-        v1.setAlpha(HIGH_OPAQUE);
-        v1.animate().setDuration(DURATION / 2).alpha(LOW_OPAQUE).setListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                v1.setAlpha(0.0f);
-                v1.setText(idOfString);
-                v1.animate().setDuration(DURATION / 2).alpha(HIGH_OPAQUE);
-                v1.animate().setListener(null);
-            }
-        });
-    }
-
-    private void toggleProgressBarVisible() {
-        progressBar.animate().setDuration(DURATION).alpha(HIGH_OPAQUE).setListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                progressBar.setVisibility(View.VISIBLE);
-            }
-        });
-    }
-
-    private void toggleProgressBarInvisible() {
-        progressBar.setVisibility(View.INVISIBLE);
-        progressBar.setAlpha(LOW_OPAQUE);
     }
 }
