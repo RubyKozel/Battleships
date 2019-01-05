48d118dc1a37e7399dd801595af96447e7bebe04
Ruby_Kozel <rubykozel@gmail.com>
2019-01-05 18:44:59 +0200

    Added delay between sensor checks

--- app/src/main/java/com/example/kozel/battleship/OrientationsSensorService.java
+++ app/src/main/java/com/example/kozel/battleship/OrientationsSensorService.java
@@ -20,6 +20,8 @@ public class OrientationsSensorService extends Service implements SensorEventLis
     private double currentValue = SensorManager.GRAVITY_EARTH;
     private double mAccel;
     private AccelerationListener mListener;
+    private boolean changed;
+    private long time;
 
     @Override
     public void onCreate() {
@@ -57,13 +59,21 @@ public class OrientationsSensorService extends Service implements SensorEventLis
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (this.mListener != null) {
-            float[] mGravity = event.values.clone();
-            double lastValue = currentValue;
-            currentValue = Math.sqrt(mGravity[0] * mGravity[0] + mGravity[1] * mGravity[1] + mGravity[2] * mGravity[2]);
-            mAccel = mAccel * 0.9f + (currentValue - lastValue);
-            if (mAccel > 3) {
-                Toast.makeText(this, "You're moving too fast! your ships got hit!", Toast.LENGTH_SHORT).show();
-                this.mListener.onAcceleration();
+            if (!changed) {
+                float[] mGravity = event.values.clone();
+                double lastValue = currentValue;
+                currentValue = Math.sqrt(mGravity[0] * mGravity[0] + mGravity[1] * mGravity[1] + mGravity[2] * mGravity[2]);
+                mAccel = mAccel * 0.9f + (currentValue - lastValue);
+                if (mAccel > 3) {
+                    Toast.makeText(this, "You're moving too fast! your ships got hit!", Toast.LENGTH_SHORT).show();
+                    this.mListener.onAcceleration();
+                    changed = true;
+                    time = System.currentTimeMillis();
+                }
+            } else {
+                Log.d("TIME", (System.currentTimeMillis() - time) + "");
+                if (System.currentTimeMillis() - time >= 3000)
+                    changed = false;
             }
         }
     }
